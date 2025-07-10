4a904bc4a5221c7f43fe5a49387904952bcb7c2c
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2025-07-10 14:41:15 +0200

    Add handling of real world units to distance to cloud + docstring.

--- src/efast_openeo/algorithms/distance_to_cloud.py
+++ src/efast_openeo/algorithms/distance_to_cloud.py
@@ -24,6 +24,47 @@ def distance_to_cloud_s2(cloud_mask: openeo.DataCube, image_size_pixels=512, max
     warnings.warn("Not yet implemented")
     return euclidean_distance_transform(cloud_mask, image_size_pixels, max_distance_pixels)
 
+def distance_to_cloud(cloud_mask: openeo.DataCube, image_size_pixels: int, *, max_distance_pixels: int | None=None, pixel_size_native_units: int | None=None, max_distance_native_units: int | None=None):
+    """
+    Compute the distance to cloud on a binary ``cloud_mask``. Distance is computed for all ``False`` pixels to all ``True`` pixels.
+    The maximum distance returned by this function can be specified either in pixels, using ``max_distance_pixels``
+    or using the native units (degrees, m, ...) of the grid, using ``max_distance_native_units``.
+    Only one of the two parameters may be specified. If given in native units, ``pixel_size_native_units`` must
+    also be specified.
+
+    The distance to cloud is returned either as a distance in pixels or as a distance in native units.
+    Native units are chosen if ``pixel_size_native_units`` is specified.
+
+    :param cloud_mask: The cloud mask (``True`` means cloud)
+    :param image_size_pixels: Chunk size for the computation. Should be larger than ``max_distance_pixels``.
+    :param max_distance_pixels: Maximum cloud distance that can be detected, given as the number of pixels from the cloud.
+    :param max_distance_native_units: Maximum cloud distance that can be detected in the native units of the raster.
+    :param pixel_size_native_units: Length of one pixel in the raster in its native units, assumed to be constant.
+        If this parameter is specified, the distance to cloud is returned in native units.
+
+    :return distance the nearest cloud (value of ``True`` in ``cloud_mask`` for each pixel that is ``False`` in
+        ``cloud_mask``, either in native units (if ``pixel_size_native_units`` is set) or in pixels otherwise.
+    """
+
+    assert (max_distance_pixels is None) ^ (pixel_size_native_units is None), (
+        "Pixel size must be specified either in pixels or in native units, not both. "
+        f"Found {max_distance_pixels=}, {max_distance_native_units=}"
+    )
+    if max_distance_native_units is not None:
+        assert (pixel_size_native_units is not None) and (pixel_size_native_units > 0), (
+            "pixel_size_in_native_units must be larger than 0 and specified if max_distance_in_native_units is set."
+        )
+        max_distance_pixels = max_distance_native_units / pixel_size_native_units
+
+    dtc_in_pixels = euclidean_distance_transform(cloud_mask, image_size_pixels=image_size_pixels, border_pixels=max_distance_pixels)
+    if pixel_size_native_units is not None:
+        return dtc_in_pixels * pixel_size_native_units
+
+    return dtc_in_pixels
+
+
+
+
 
 def distance_to_cloud_s3(cloud_mask: openeo.DataCube, image_size_pixels=512, max_distance_pixels=255):
     """
