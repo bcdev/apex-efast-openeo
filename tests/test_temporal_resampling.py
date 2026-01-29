1c006596b38637607dace7565d4abdd8702cab4e
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-01-29 12:57:24 +0100

    Update temporal resampling tests to new interface

--- tests/test_temporal_resampling.py
+++ tests/test_temporal_resampling.py
@@ -22,12 +22,17 @@ def s3_at_s2_times_mock(s3_cube, s2_time_series):
 
 @pytest.mark.openeo
 @pytest.mark.manual
-def test_merge_cubes(s3_cube, s2_time_series, persistent_output_dir, run_openeo):
+def test_merge_cubes(s3_cube, time_frame, time_frame_target, interval_days, persistent_output_dir, run_openeo):
     """
     This test uses S3 input products instead of the S3 composites used by efast.
     The procedure for temporal resampling should be the same, as the composite cube
     should emulate the original S3 cube exactly.
     """
 
-    s3_at_s2_time = interpolate_time_series_to_target_extent(s3_cube, s2_time_series)
+    s3_at_s2_time = interpolate_time_series_to_target_extent(
+        s3_cube,
+        temporal_extent=time_frame,
+        temporal_extent_target=time_frame_target,
+        interval_days=interval_days,
+    )
     run_openeo(s3_at_s2_time, persistent_output_dir / "s3_at_s2_time")
